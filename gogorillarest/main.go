e89556b76ac04c18fbc6fb6b77540265542c1eb2
CristianQS <cristian_31teror@hotmail.com>
2021-09-16 20:22:12 +0100

    remove redundant endpoint

--- gogorillarest/main.go
+++ gogorillarest/main.go
@@ -15,12 +15,6 @@ var (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/helloworld", HelloWorldHandler())
-	r.HandleFunc("/helloworld/{name}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		fmt.Fprintf(w, "Hello %s, you've requested: %s\n", vars["name"], r.URL.Path)
-		fmt.Printf("It was requested: %s\n\n", r.URL.Path)
-	})
 	r.HandleFunc("/configuration/{id}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		repository.Set(vars["id"], "hello")
@@ -32,14 +26,3 @@ func main() {
 	}).Methods(http.MethodGet)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
-
-func HelloWorldHandler() func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-		fmt.Printf("It was requested: %s\n\n", r.URL.Path)
-	}
-}
-
-
-
-
