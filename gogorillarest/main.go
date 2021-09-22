95d07014b822d9c3fa42fd74562b471f4d8709db
CristianQS <cristian_31teror@hotmail.com>
2021-09-23 00:17:28 +0100

    refactor: extarc class

--- gogorillarest/main.go
+++ gogorillarest/main.go
@@ -18,23 +18,31 @@ var (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/configuration/{id}", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/configuration/{id}", GetConfigMapById()).Methods(http.MethodPost)
+	r.HandleFunc("/configuration/{id}", AddConfigMap()).Methods(http.MethodGet)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func AddConfigMap() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		configMap := repository.GetById(vars["id"])
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(configMap)
+	}
+}
+
+func GetConfigMapById() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		bytes, err := ioutil.ReadFile("testdata/configmap.yaml")
 		if err != nil {
 			log.Printf("yamlFile.Get err   #%v ", err)
 		}
-		service := configmap.NewService(yaml.YamlV2Serializer{},*repository)
-		service.GetConfigMap(vars["id"],bytes)
+		service := configmap.NewService(yaml.YamlV2Serializer{}, *repository)
+		service.GetConfigMap(vars["id"], bytes)
 		w.WriteHeader(http.StatusCreated)
-	}).Methods(http.MethodPost)
-	r.HandleFunc("/configuration/{id}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		configMap := repository.GetById(vars["id"])
-		w.Header().Set("Content-Type", "application/json")
-		_ = json.NewEncoder(w).Encode(configMap)
-	}).Methods(http.MethodGet)
-	log.Fatal(http.ListenAndServe(":8080", r))
+	}
 }
 
 
